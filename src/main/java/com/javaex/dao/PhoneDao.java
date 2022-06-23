e0c959e6230fc2b90a982181955704e67ed53fd6
5335510 <5335510@naver.com>
2022-06-23 09:45:23 +0900

    2022 06-23 09:45

--- src/main/java/com/javaex/dao/PhoneDao.java
+++ src/main/java/com/javaex/dao/PhoneDao.java
@@ -9,77 +9,76 @@ import org.springframework.stereotype.Repository;
 
 import com.javaex.vo.PersonVo;
 
-@Repository //Autowired에 연결
+@Repository // Autowired에 연결
 public class PhoneDao {
-	
-	@Autowired
-	private SqlSession sqlSession; //미리 여러개를 연결해놈
-	
 
-	//등록
+	@Autowired
+	private SqlSession sqlSession; // 미리 여러개를 연결해놈
+
+	// 등록
 	public int personInsert(PersonVo personVo) {
 		System.out.println("PhoneDao>>personInsert");
-		
+
 		int count = sqlSession.insert("phonebook.personInsert", personVo);
-		
+
 		return count;
 	}
-	
-	//등록(map)
+
+	// 등록(map)
 	public int personInsert2(Map<String, String> pMap) {
 		System.out.println("PhoneDao>>personInsert2");
-		
-		//key값
+
+		// key값
 		pMap.get("name");
 		pMap.get("hp");
 		pMap.get("company");
 		int count = sqlSession.insert("phonebook.personInsert2", pMap);
-		
+
 		return count;
 	}
-	
-	//수정	
+
+	// 수정
 	public int personUpdate(PersonVo personVo) {
 		System.out.println("PhoneDao>>personInsert");
-		
+
 		int count = sqlSession.update("phonebook.personUpdate", personVo);
-		
+
 		return count;
 	}
-	
-	//삭제
+
+	// 삭제
 	public int personDelete(int personId) {
 		System.out.println("PhoneDao>>personInsert");
-		
+
 		int count = sqlSession.delete("phonebook.personDelete", personId);
 		return count;
 	}
-	
-	//리스트
+
+	// 리스트
 	public List<PersonVo> getPersonList() {
 		System.out.println("PhoneDao>>getPersonList");
-		
+
 		List<PersonVo> personList = sqlSession.selectList("phonebook.selectList");
 		System.out.println(personList);
-		
+
 		return personList;
 	}
-	//한사람 가져오기	
+
+	// 한사람 가져오기
 	public PersonVo getPerson(int personId) {
 		System.out.println("PhoneDao>>getPerson");
-		
-		return sqlSession.selectOne("phonebook.getPerson",personId);
-		
+
+		return sqlSession.selectOne("phonebook.getPerson", personId);
+
 	}
-	//한사람 가져오기(map)
-	public PersonVo getPerson2(int personId) {
-		System.out.println("PhoneDao > getPerson2()");
-		
-		Map<String,Object> pMap = sqlSession.selectOne("phonebook.getPerson2",personId);
-		System.out.println(pMap.toString());
-		
-		return null;
-		
-		
+
+	// 한사람 가져오기(map)
+	public Map<String, Object> getPerson2(int no) {
+		System.out.println("PhoneDao>getPerson2()");
+		System.out.println(no);
+
+		Map<String, Object> pMap = sqlSession.selectOne("phonebook.getPerson2", no);
+
+		return pMap;
 	}
 }
--- src/main/java/com/javaex/service/PhoneService.java
+++ src/main/java/com/javaex/service/PhoneService.java
@@ -12,71 +12,75 @@ import com.javaex.vo.PersonVo;
 
 @Service
 public class PhoneService {
-	
-	//필드
+
+	// 필드
 	@Autowired
 	private PhoneDao phoneDao;
-	//생성자
-	
-	//메소드 gs
-	
-	//메소드 일반
-	
-	//전화번호 리스트
-	public List<PersonVo> getPersonList(){
-		//코드작성
+	// 생성자
+
+	// 메소드 gs
+
+	// 메소드 일반
+
+	// 전화번호 리스트
+	public List<PersonVo> getPersonList() {
+		// 코드작성
 		List<PersonVo> personList = phoneDao.getPersonList();
-		
+
 		return personList;
 	}
-	
-	//전화번호 등록
+
+	// 전화번호 등록
 	public int personInsert(PersonVo personVo) {
-		
+
 		int count = phoneDao.personInsert(personVo);
-		
+
 		return count;
 	}
-	
-	//전화번호 등록(map 사용)
+
+	// 전화번호 등록(map 사용)
 	public int personInsert2() {
 		System.out.println("PhoneService>>personInsert2()");
-		
-		//map을 만들어 사용한다
+
+		// map을 만들어 사용한다
 		Map<String, String> pMap = new HashMap<String, String>();
 		pMap.put("name", "황일영");
 		pMap.put("hp", "010-1111-1111");
 		pMap.put("company", "02-222-2222");
-		
+
 		int count = phoneDao.personInsert2(pMap);
 		return count;
 	}
-	
-	//전화번호 삭제
+
+	// 전화번호 삭제
 	public int personDelete(int no) {
-		
+
 		int count = phoneDao.personDelete(no);
 		return count;
 	}
-	
-	//전화번호 수정폼(정보가져오기)
+
+	// 전화번호 수정폼(정보가져오기)
 	public PersonVo getPerson(int no) {
-		
+
 		PersonVo personVo = phoneDao.getPerson(no);
-		
+
 		return personVo;
 	}
-	
-	//전화번호 수정폼(정보가져오기, map방식)
-	public PersonVo getPerson2(int no) {
-		
+
+	// 전화번호 수정폼(정보가져오기, map방식)
+	public Map<String, Object> getPerson2(int no) {
+		System.out.println("PhoneService>getPerson2()");
+
+		Map<String, Object> pMap = phoneDao.getPerson2(no);
+
+		return pMap;
 	}
-	
-	//전화번호 수정
+
+	// 전화번호 수정
 	public int personUpdate(PersonVo personVo) {
-		
+
 		int count = phoneDao.personUpdate(personVo);
-		
+
 		return count;
 	}
 
